39611cc44931a2347b31e2d8dd3c5c38fd14915f
jmartinezpineiro <jmartinezpineiro@nautilus16.danielcastelao.org>
2018-11-22 09:21:30 +0100

    Esta to fusionao, salu2. Soy programador

---
+++ src/cod_colaboration/ClassLeader.java
@@ -0,0 +1,21 @@
+package cod_colaboration;
+
+
+/**
+ *
+ * @author jmartinezpineiro
+ */
+public class ClassLeader {
+
+    class Leader {
+        int lider1;
+        int lider2;
+        //Saludos a la GC
+    }
+    
+    class VivaElLider{
+        int FidelCastro;
+        int WinstonChurchill;
+        //**persona andando**
+    }
+}
