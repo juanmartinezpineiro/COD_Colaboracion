76aff94f50c92f242c6a5d3ae33cc00507ae6786
jmartinezpineiro <jmartinezpineiro@nautilus16.danielcastelao.org>
2018-11-22 08:56:05 +0100

    Primer commit del master

---
+++ src/cod_colaboration/COD_Colaboration.java
@@ -0,0 +1,23 @@
+package cod_colaboration;
+
+/**
+ *
+ * @author jmartinezpineiro
+ */
+public class COD_Colaboration {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+int leader1;
+int leador2;
+int suma;
+
+suma = leader1+leader2;
+
+
+    }
+
+}
