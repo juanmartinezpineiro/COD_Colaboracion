226233d0dd841ddf919736252a14fc2eda5215fa
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2018-11-27 09:22:22 +0100

    1.clase nueva colaborador_request

---
+++ src/cod_colaboration/Colaborador_Request.java
@@ -0,0 +1,13 @@
+/*
+ * En esta clase vamos a probar el pull request
+para ello hacemos una rama y luego la subimos al github 
+ */
+package cod_colaboration;
+
+/**
+ *
+ * @author lvazquezdorna
+ */
+public class Colaborador_Request {
+    
+}
