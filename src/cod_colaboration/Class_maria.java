4ee9e346dc89a69066036aeba4c110f6ccc0e514
mrebelladofernandez <mrebelladofernandez@nautilus18.danielcastelao.org>
2018-11-27 09:50:44 +0100

    first commit en la rama

---
+++ src/cod_colaboration/Class_maria.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cod_colaboration;
+
+/**
+ *
+ * @author mrebelladofernandez
+ */
+public class Class_maria {
+    float clase;
+    float silla;
+    
+}
